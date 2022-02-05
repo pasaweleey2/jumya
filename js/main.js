4b50b22e53da9851235ff4d11b2749a6fc4847b7
pasaweleey2 <98903495+pasaweleey2@users.noreply.github.com>
2022-02-05 12:02:09 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,239 @@
+
+
+
+
+
+
+
+
+
+// Function Display 
+function myFunc() {
+    var x = document.getElementById("div1");
+
+    if(x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+var x = 10;
+
+if(x == 20) {
+    console.log("hello 10"); 
+} else {
+    console.log("hello xxxx"); 
+}
+
+// Function Open Nav 
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(158, 158, 158, 0.363)";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+    document.body.style.backgroundColor = "white";
+}
+
+// Function DarkMode
+function DarkMode() {
+    document.body.classList.toggle("dark23");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("hello"); هو الحزيرhelloلعمل تحزير علي بداية خول الموقع و مكان 
+
+//document.write(3*10);
+
+//console.log("mohamed");  inspect في ال pageبتظهر ال
+
+//document.getElementById("demo").innerHTML=" I am Mohamed ohamed Full stack Developer";
+
+//Varaible
+
+//var------const------let-----
+
+//data type
+
+//strng number object array undefeind null boolean 
+
+//var Name = "mohamed"; //strng 
+//var myage = 15; //number
+//var name = true; //boolean 
+//var name = false; //boolean 
+//var product = ["mohamed", 15]
+//console.log(product[0])
+//console.log(product[1])
+//console.log(Name) ;
+//console.log(myage);
+//var product ={ Type: "shirt", Color: "red",  Size: "m"}
+//console.log(product.Size);//dot notain
+ 
+//=   ==   ===
+//var x=10
+//var x=100 number 
+//var y=100 number x===y
+
+//-  +  /  *
+
+//var x=100;
+//var y=200;
+//var z=x+y
+//var z=x-y
+//var z=x/y
+//var z=x*y
+//cosole.log(z)300;
+//cosole.log(z)100-;
+//cosole.log(z)2;
+//cosole.log(z)20000;
+
+
+
+
+// varaible
+
+//1- var ------ 2-const ---- 3-let 
+
+
+// Datatype 
+
+// String 
+// number
+// boolean
+// array
+// object
+// undefeind
+// null  
+
+// var x = 10; // number 
+
+// var Name = "Alaa"; // String
+
+// var nameeee = true; // boolean
+
+// var Products = ["Product1", "Product2", "Product3"]; // index
+
+// console.log(Products[3]);
+
+// object
+// var Products = {
+//     property : "value",
+//     property1 : "value1",
+// }
+
+// var Products = {
+//     Type: "shirt",
+//     Color: "red",
+//     Size: "M"
+// }
+
+// console.log(Productsss.Color); // dot notain
+
+// document.getElementById("alaa").innerHTML = "My Type Is: " + Productsss.Type;
+
+// var x = null; 
+
+// console.log(x);
+
+// =   ==  === 
+
+// var x = null;
+
+// console.log(typeof x);
+
+
+// operators
+// + - / *
+
+// var x  = 100;
+
+// var y = 200;
+
+// var z = x + y;
+// var z = x - y;
+// var z = x / y;
+// var z = x * y;
+
+// console.log(z); // 300
+// console.log(z); // -100
+// console.log(z); // 2
+// console.log(z); // 2000
+
+
+// Increment ++
+// Decrement --
+
+// console.log(2++);
+// console.log(2--);
+
+
+// = 
+
+// == 
+
+// != 
+
+// ===
+// type value 
+
+// !== 
+
+// not type not value 
+
+// 0 >= 0
+
+// 10 >= 100
+
+// &&  and 
+
+// || or 
+
+
+// date 
+
+// 1 jan 1970
+
+// var date = new Date();
+
+// console.log(date.getTime());
+// // getFullYear 1930
+
+// // getMonth 0 - 11
+
+// // getDate  1 - 31
+
+// // getDay 0 - 6 
+
+// // getHours 0 - 23 
+
+// // getMinutes 0 - 59 
+
+// // getSeconds 0 - 59
+
+// // getMilliseconds 0 - 999
+
+// // getTime 1 jan 1970 
+
+
+
+
